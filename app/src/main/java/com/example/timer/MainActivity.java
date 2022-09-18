d2e2d244cf07e2582b54cf0b649e659c193c0929
lovan <lovan_zhang@163.com>
2022-09-18 21:19:39 +0800

    finish time

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.timer;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
-import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.*;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,17 +17,34 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Time;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     String msg="Android:";
-    private Button button1;
-    private EditText loop;
+    private Button startBut;
+    private Button stopBut;
+    //倒计时时间 s
+    private EditText loopTime;
+    private Integer loopTime2;
+    private Long loopTime3;
+    private Integer tmpLoop;
+    //间隔时间 s
     private EditText external;
-    private TextView time1;
+    private Integer external2;
+    private Long external3;
+    private Integer tmpExternal;
+    //显示框
+    private TextView showTime;
 
+    private Timer timer=new Timer();
+    private TimerTask timerTask;
+    private MediaPlayer go;
+    private MediaPlayer rest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,37 +53,72 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        loop=(EditText) findViewById(R.id.loop);
-        external=(EditText) findViewById(R.id.external);
-        time1=(TextView)findViewById(R.id.time);
+        startBut=(Button) findViewById(R.id.button);
+        stopBut=(Button) findViewById(R.id.button2);
 
-        final String test=external.getText().toString();
-        button1=(Button) findViewById(R.id.button);
-        button1.setOnClickListener(new View.OnClickListener() {
+//        go=MediaPlayer.create(this,R.raw.go);
+//        rest=MediaPlayer.create(this,R.raw.rest);
+//        Log.i(msg,go.toString());
+//        Log.i(msg,rest.toString());
+
+        startBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Integer second=Integer.parseInt(loop.getText().toString());
-                Long mill=(long)(second*1000);
+                loopTime=(EditText) findViewById(R.id.loop);
+                loopTime2=Integer.parseInt(loopTime.getText().toString());
+                tmpLoop=loopTime2;
+                external=(EditText) findViewById(R.id.external);
+                external2=Integer.parseInt(external.getText().toString());
+                tmpExternal=external2;
+                showTime=(TextView)findViewById(R.id.time);
+                timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        if (loopTime2 >= 0) {
+//                            try{
+//                                mediaPlayer.prepare();
+//                                mediaPlayer.start();
+//                            }catch (IOException e){
+//                                Log.i(msg,"media player error:"+e.getMessage());
+//                            }
+                            String str = "<font color='#39d916'>" + loopTime2 + "</font>";
+                            showTime.setText(Html.fromHtml(str));
+                            loopTime2 -= 1;
+                        } else {
+                            String str = "<font color='#d63b1c'>" + external2 + "</font>";
+                            if (external2 > 0) {
+                                Log.i(msg, "444 loopTime2:" + loopTime2 + " external2:" + external2);
+                                showTime.setText(Html.fromHtml(str));
+                                external2 -= 1;
+                            } else {
+                                Log.i(msg, "555 loopTime2:" + loopTime2 + " external2:" + external2);
+                                showTime.setText(Html.fromHtml(str));
+                                loopTime2 = tmpLoop;
+                                external2 = tmpExternal;
 
-
-                for (int i=0;i<5;i++){
-                    new CountDownTimer(mill,1000){
-                        public void onTick(long mill){
-                            button1.setClickable(false);
-                            time1.setText(mill/1000+"s");
+                            }
                         }
-
-                        public void onFinish(){
-                            button1.setClickable(true);
-                        }
-                    }.start();
-                }
+                    }
+                };
+                startBut.setClickable(false);
+                startBut.setVisibility(View.INVISIBLE);
+                stopBut.setVisibility(View.VISIBLE);
+                timer.schedule(timerTask,0,1000);
             }
         });
 
-        Log.d(msg,"onCreate");
-        Log.d(msg,loop.getText().toString());
-        Log.d(msg,external.getText().toString());
+        stopBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(msg,"stop button");
+                startBut.setClickable(true);
+                timerTask.cancel();
+                startBut.setVisibility(View.VISIBLE);
+                stopBut.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        Log.i(msg,"onCreate");
     }
 
     @Override
